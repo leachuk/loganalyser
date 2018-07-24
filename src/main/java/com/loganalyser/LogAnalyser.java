b1ffa131b4138ba09d6beb3cf0fcf91ea86ae60c
stewart leach <stew.leach@gmail.com>
2018-07-24 20:47:46 +1000

    cleanup

--- src/main/java/com/loganalyser/LogAnalyser.java
+++ src/main/java/com/loganalyser/LogAnalyser.java
@@ -12,16 +12,12 @@ public class LogAnalyser {
     private static final int SEQUENTIAL_PAGE_COUNT = 3;
     
     public static void main(String[] args) {
-        System.out.println("Welcome to the Log Analyser");
-
-         //group pages by user
-        Map<String, List<LogEntryItem>> tempDataStore = getSampleData().collect(Collectors.groupingBy(LogEntryItem::getUser));
+        System.out.println("================== Welcome to the Log Analyser ==================");
         
-        //get 3-page view groups
-        //Map<String, List<List<String>>> sequentialPages = new TreeMap<>();
+        Map<String, List<LogEntryItem>> dataStore = getSampleData().collect(Collectors.groupingBy(LogEntryItem::getUser));
+        
         Map<Integer, Log> logMap = new HashMap<>();
-        for (Map.Entry<String, List<LogEntryItem>> user : tempDataStore.entrySet()) {
-            //List tempLogList = new ArrayList<String>();
+        for (Map.Entry<String, List<LogEntryItem>> user : dataStore.entrySet()) {
             for (int i = 0; i <= user.getValue().size() - SEQUENTIAL_PAGE_COUNT; i++){
                 Map<String, List<String>> tempPageList = subList(user.getValue(), i,SEQUENTIAL_PAGE_COUNT)
                                                          .stream()
@@ -37,48 +33,20 @@ public class LogAnalyser {
                     Log existingLog = logMap.get(tempPageList.get(user.getKey()).hashCode());
                     existingLog.setOccurrences(existingLog.getOccurrences() + 1);
                 }
-                
-                //tempLogList.add(tempPageList.get(user.getKey()));
             }
-            //sequentialPages.put(user.getKey(), tempLogList);
         }
-    
-        //Object test = sequentialPages.entrySet().stream().collect(Collectors.toMap(o -> o.getKey(), e -> doProcess(e.getValue())));
-        //List<List<String>> testFlatMap = sequentialPages.entrySet().stream().flatMap(o -> o.getValue().stream()).collect(Collectors.toList());
-        //Object testDistinct = sequentialPages.entrySet().stream().flatMap(o -> o.getValue().stream()).distinct().collect(Collectors.toList());
-    
-        //Object testSize = testFlatMap.stream().collect(Collectors.groupingBy(o -> o.toString()));
-
-        //oldschool loop to get duplicate count
-//        Map<Integer, Log> logMap = new HashMap<>();
-//        for (List itemList : testFlatMap) {
-//            if (!logMap.containsKey(itemList.hashCode())) {
-//                LogEntryList logEntryList = new LogEntryList(itemList);
-//                Log log = new Log(logEntryList);
-//                log.setOccurrences(1);
-//                logMap.put(itemList.hashCode(), log);
-//            } else {
-//                Log existingLog = logMap.get(itemList.hashCode());
-//                existingLog.setOccurrences(existingLog.getOccurrences() + 1);
-//            }
-//        }
-
-        //output top 2 journeys by occurrence
+        
         List<Map.Entry> ordered = logMap.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue((o1, o2) -> o2.getOccurrences().compareTo(o1.getOccurrences())))
                 .limit(10)
                 .collect(Collectors.toList());
 
-//        for (Map.Entry<Integer, Log> map : ordered) {
-//            System.out.println(map.getValue().getPageJourney().printList() + String.format("[%d]", map.getValue().getOccurrences()));
-//        }
+        for (Map.Entry<Integer, Log> map : ordered) {
+            System.out.println(map.getValue().getPageJourney().printList() + String.format("\t[%d]", map.getValue().getOccurrences()));
+        }
 
-        System.out.println("Completed Log Analyser");
-    }
-
-    private static String doProcess(List list){
-        return list.toString() + ":" + list.hashCode();
+        System.out.println("================== Completed Log Analyser ==================");
     }
 
     private static Stream<LogEntryItem> getSampleData(){
@@ -102,14 +70,6 @@ public class LogAnalyser {
         );
 
         return logItemList;
-    }
-
-    private static void printToConsole(List<String> transactions) {
-        System.out.println("===========================================");
-        for (int i = 0; i < transactions.size(); i++) {
-            System.out.println("["+ i +"] " + transactions.get(i));
-        }
-        System.out.println("===========================================");
     }
 
     private static <T> List<T> subList(List<T> list, int fromIndex, int returnListSize) {
